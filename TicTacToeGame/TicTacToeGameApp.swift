2a7b5c50c3f6e5bc81d90630d0ff455c436b7815
Artem Paliutin <pal.artem@icloud.com>
2022-06-21 14:22:11 +0300

    Initial Commit

---
+++ TicTacToeGame/TicTacToeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeGameApp.swift
+//  TicTacToeGame
+//
+//  Created by Artem Paliutin on 21/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
